c30c0868bfa29e5075686792e1cb7fdb573cdff2
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-08-19 17:30:09 -0600

    app-5 completed

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Image url={'https://th.bing.com/th/id/OIP.43Pu_Cn9ZtZnrNcmKfpfTwHaEK?w=331&h=186&c=7&o=5&dpr=1.1&pid=1.7'} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
